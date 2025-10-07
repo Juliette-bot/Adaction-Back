68f6864ef5e9574c017b7af6ae6a99cf104d4a2e
Juliette <jblanc17@outlook.fr>
2025-10-07 17:36:55 +0200

    feat: add model and controller display waste on java

--- backend/src/main/java/com/adaction/backend/controller/ControllerdDisplayWaste.java
+++ backend/src/main/java/com/adaction/backend/controller/ControllerdDisplayWaste.java
@@ -1,34 +1,43 @@
-package com.adaction.backend.modele;
+package com.adaction.backend.controller;
+import com.adaction.backend.modele.ModelDisplayWaste;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @RestController
-public class ModeleDisplayWaste {
+@CrossOrigin(origins = "http://localhost:5173")
+public class ControllerdDisplayWaste {
 
     private static final String URL = "jdbc:mysql://localhost:3306/BDDAdaction";
     private static final String USER = "root";
     private static final String PASSWORD = "Juliette17!";
+
+
     @GetMapping("/waste")
-    public List<String> getWaste() {
-        List<String> waste = new ArrayList<>();
+    public List<ModelDisplayWaste> getWaste() {
+        List<ModelDisplayWaste> wastes = new ArrayList<>();
 
         try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
              Statement smt = con.createStatement();
              ResultSet rs = smt.executeQuery("SELECT * FROM waste")) {
 
             while (rs.next()) {
-                waste.add(rs.getString("name"));
-
+                ModelDisplayWaste waste = new ModelDisplayWaste(
+                        rs.getInt("id"),
+                        rs.getString("type"),
+                        rs.getString("name"),
+                        rs.getInt("point"),
+                        rs.getString("icone")
+                );
+                wastes.add(waste);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    return waste;
-    }
 
+        return wastes;
+    }
 }
