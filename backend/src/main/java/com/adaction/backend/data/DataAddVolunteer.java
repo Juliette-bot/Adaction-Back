3ccb58ef41526ff6dada7089dc51e80429482a68
Victoria Ferry <ferryvictoria@hotmail.fr>
2025-10-08 16:17:48 +0200

    update : post rout from front to back ok

--- backend/src/main/java/com/adaction/backend/data/DataAddVolunteer.java
+++ backend/src/main/java/com/adaction/backend/data/DataAddVolunteer.java
@@ -1,6 +1,6 @@
 package com.adaction.backend.data;
 
-import com.adaction.backend.model.ModelChangeVolunteerVueAdmin;
+import com.adaction.backend.modele.ModelChangeVolunteerVueAdmin;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
@@ -26,6 +26,21 @@ public class DataAddVolunteer {
             stmt.setInt(6, 0);
 
             stmt.executeUpdate();
+    public void insertVolunteer(ModelChangeVolunteerVueAdmin volunteer) throws SQLException {
+        String sql = "INSERT INTO volunteer (firstName, lastName, email, pass_word, city_id, points) VALUES (?, ?, ?, ?, (SELECT id FROM city WHERE city = ?), ?)";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, volunteer.getFirstName());
+            stmt.setString(2, volunteer.getLastName());
+            stmt.setString(3, volunteer.getEmail());
+            stmt.setString(4, volunteer.getPass_word());
+            stmt.setString(5, volunteer.getCity_id());
+            stmt.setInt(6, 0);
+
+            stmt.executeUpdate();
         }
     }
+}
 }
